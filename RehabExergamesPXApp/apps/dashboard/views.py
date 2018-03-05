a249c5e99e5bfe447bd8a0d5ca68d8347fd119b8
edwingamboa <edwingamboa355@gmail.com>
2018-03-05 09:49:13 -0500

    #2: Add dashboard app

---
+++ RehabExergamesPXApp/apps/dashboard/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+# Create your views here.
+class Dashboard(TemplateView):
+    template_name = "dashboard/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['welcome'] = 'Welcome'
+        return context
