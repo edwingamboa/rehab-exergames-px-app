7358ab555ba927ea8673f12728127f7b189c3b94
edwingamboa <edwingamboa355@gmail.com>
2018-03-05 08:43:59 -0500

    #2: Add base dashboard app

--- RehabExergamesPXApp/RehabExergamesPXApp/settings.py
+++ RehabExergamesPXApp/RehabExergamesPXApp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.dashboard',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'RehabExergamesPXApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),
+)
+#esta linea la cambie tenia static_collected
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
