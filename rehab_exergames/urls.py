f60ba08d22d738adb22f6ac8ef5fc20e7ae18a9f
edwingamboa <edwingamboa355@gmail.com>
2018-03-26 17:02:00 -0500

    Fix urls for rehabilitation tasks

--- rehab_exergames/urls.py
+++ rehab_exergames/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)$', RehabilitationExergameDetail.as_view(), name='detail'),
     url(r'^new', RehabilitationExergameCreation.as_view(), name='new'),
     url(r'^update/(?P<pk>\d+)$', RehabilitationExergameUpdate.as_view(), name='update'),
-    url(r'^rehab_exergame/$', RehabilitationTaskList.as_view(), name='list_task'),
-    url(r'^rehab_exergame/(?P<pk>\d+)$', RehabilitationTaskDetail.as_view(), name='detail_task'),
-    url(r'^rehab_exergame/new', RehabilitationTaskCreation.as_view(), name='new_task'),
-    url(r'^rehab_exergame/update/(?P<pk>\d+)$', RehabilitationTaskUpdate.as_view(), name='update_task'),
+    url(r'^rehab_task/$', RehabilitationTaskList.as_view(), name='list_task'),
+    url(r'^rehab_task/(?P<pk>\d+)$', RehabilitationTaskDetail.as_view(), name='detail_task'),
+    url(r'^rehab_task/new', RehabilitationTaskCreation.as_view(), name='new_task'),
+    url(r'^rehab_task/update/(?P<pk>\d+)$', RehabilitationTaskUpdate.as_view(), name='update_task'),
 ]
