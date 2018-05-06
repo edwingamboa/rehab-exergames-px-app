3d22b3df0cdbcbb6dc4e48b4718a1b4e55c475a2
edwingamboa <edwingamboa355@gmail.com>
2018-05-06 13:42:31 -0500

    #27: Add resources urls to main app urls

--- RehabExergamesPXApp/urls.py
+++ RehabExergamesPXApp/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,4 +25,9 @@ urlpatterns = [
     url(r'^movements/', include('movements.urls', namespace='movements')),
     url(r'^pathologies/', include('pathologies.urls', namespace='pathologies')),
     url(r'^rehab_exergames/', include('rehab_exergames.urls', namespace='rehab_exergames')),
+    url(r'^resources/', include('resources.urls', namespace='resources')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
