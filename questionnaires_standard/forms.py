e7cef3988795a91b5e9da8f11772ee08f1394cd8
edwingamboa <edwingamboa355@gmail.com>
2018-05-30 18:31:34 -0500

    #35: Add form for QuestionnaireUpdate view

--- questionnaires_standard/forms.py
+++ questionnaires_standard/forms.py
@@ -3,6 +3,48 @@ from .models import Questionnaire
 from utilities.constants import Constants
 
 
+class QuestionnaireContinueCreationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(QuestionnaireContinueCreationForm, self).__init__(*args,**kwargs)
+
+        instance = kwargs.get('instance', None)
+        if instance:
+            if instance.status == Constants.INIT:
+                del self.fields['description']
+                del self.fields['target_respondents']
+                del self.fields['aspects']
+                del self.fields['questionnaire_document']
+                del self.fields['additional_documents']
+                del self.fields['validity']
+                del self.fields['validity_measure']
+                del self.fields['reliability']
+                del self.fields['reliability_measure']
+                del self.fields['pre_testing_report']
+            elif instance.status == Constants.IN_DESIGN:
+                del self.fields['name']
+                del self.fields['target_respondents']
+                del self.fields['aspects']
+                del self.fields['evaluation_objective']
+                del self.fields['validity']
+                del self.fields['validity_measure']
+                del self.fields['reliability']
+                del self.fields['reliability_measure']
+                del self.fields['pre_testing_report']
+            elif instance.status == Constants.IN_PRE_TEST or instance.status == Constants.FINISHED:
+                del self.fields['evaluation_objective']
+                del self.fields['target_respondents']
+                del self.fields['aspects']
+                del self.fields['name']
+                del self.fields['description']
+                del self.fields['questionnaire_document']
+                del self.fields['additional_documents']
+
+    class Meta:
+        model = Questionnaire
+        fields = '__all__'
+        exclude = ('status', 'methods', 'resources')
+
+
 class QuestionnaireUpdateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(QuestionnaireUpdateForm, self).__init__(*args,**kwargs)
@@ -10,7 +52,6 @@ class QuestionnaireUpdateForm(forms.ModelForm):
         instance = kwargs.get('instance', None)
         if instance:
             if instance.status == Constants.INIT:
-                del self.fields['name']
                 del self.fields['description']
                 del self.fields['questionnaire_document']
                 del self.fields['additional_documents']
@@ -20,22 +61,11 @@ class QuestionnaireUpdateForm(forms.ModelForm):
                 del self.fields['reliability_measure']
                 del self.fields['pre_testing_report']
             elif instance.status == Constants.IN_DESIGN:
-                del self.fields['evaluation_objective']
-                del self.fields['target_respondents']
-                del self.fields['aspects']
                 del self.fields['validity']
                 del self.fields['validity_measure']
                 del self.fields['reliability']
                 del self.fields['reliability_measure']
                 del self.fields['pre_testing_report']
-            elif instance.status == Constants.IN_PRE_TEST:
-                del self.fields['evaluation_objective']
-                del self.fields['target_respondents']
-                del self.fields['aspects']
-                del self.fields['name']
-                del self.fields['description']
-                del self.fields['questionnaire_document']
-                del self.fields['additional_documents']
 
     class Meta:
         model = Questionnaire
