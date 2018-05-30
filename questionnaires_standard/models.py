411b0fc689e4d1984d04727a3926df3336fcc726
edwingamboa <edwingamboa355@gmail.com>
2018-05-30 18:31:11 -0500

    #35: Fix models

--- questionnaires_standard/models.py
+++ questionnaires_standard/models.py
@@ -23,13 +23,13 @@ class Measure(models.Model):
 class Questionnaire(Instrument):
     evaluation_objective = models.TextField()
     target_respondents = models.TextField()
-    questionnaire_document = models.ForeignKey(Resource, related_name='%(class)s_questionnaire_doc')
-    additional_documents = models.ManyToManyField(Resource, related_name='%(class)s_additional_docs')
+    questionnaire_document = models.ForeignKey(Resource, related_name='%(class)s_questionnaire_doc', null=True)
+    additional_documents = models.ManyToManyField(Resource, related_name='%(class)s_additional_docs', null=True)
     validity = models.CharField(max_length=140)
-    validity_measure = models.ForeignKey(Measure, related_name='%(class)s_validity')
+    validity_measure = models.ForeignKey(Measure, related_name='%(class)s_validity', null=True)
     reliability = models.CharField(max_length=140)
-    reliability_measure = models.ForeignKey(Measure, related_name='%(class)s_reliability')
-    pre_testing_report = models.ForeignKey(Resource, related_name='%(class)s_pre_test_report')
+    reliability_measure = models.ForeignKey(Measure, related_name='%(class)s_reliability', null=True)
+    pre_testing_report = models.ForeignKey(Resource, related_name='%(class)s_pre_test_report', null=True)
     status = models.CharField(
         max_length=20,
         default=Constants.INIT,
