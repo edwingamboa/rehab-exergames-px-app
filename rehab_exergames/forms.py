9c7dd8cedc5250fad5d957a8addc3b6bf16e7b38
edwingamboa <edwingamboa355@gmail.com>
2018-05-30 19:29:05 -0500

    #29: Define queryset in __init__ method

--- rehab_exergames/forms.py
+++ rehab_exergames/forms.py
@@ -6,12 +6,14 @@ from django.db.models import Q
 
 
 class RehabilitationExergameForm(forms.ModelForm):
-    input_interaction_devices = forms.ModelMultipleChoiceField(queryset=InteractionDevice.objects.filter(
-        Q(type=Constants.INPUT_DEVICE) | Q(type=Constants.IN_OUTPUT_DEVICE)
-    ))
-    output_interaction_devices = forms.ModelMultipleChoiceField(queryset=InteractionDevice.objects.filter(
-        Q(type=Constants.OUTPUT_DEVICE) | Q(type=Constants.IN_OUTPUT_DEVICE)
-    ))
+    def __init__(self, *args, **kwargs):
+        super(RehabilitationExergameForm, self).__init__(*args, **kwargs)
+        self.fields['input_interaction_devices'].queryset = InteractionDevice.objects.filter(
+            Q(type=Constants.INPUT_DEVICE) | Q(type=Constants.IN_OUTPUT_DEVICE)
+        )
+        self.fields['output_interaction_devices'].queryset = InteractionDevice.objects.filter(
+            Q(type=Constants.OUTPUT_DEVICE) | Q(type=Constants.IN_OUTPUT_DEVICE)
+        )
 
     class Meta(object):
         model = RehabilitationExergame
